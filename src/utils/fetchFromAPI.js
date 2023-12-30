bfb525123132b66dc99e0b9dba845f23710944cf
Dakshayani511 <dakshayaniyadav7@gmail.com>
2023-12-30 18:37:13 +0530

    done

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "b6ca2fa60amsh983c3d229ba187fp14e60cjsn882527198789",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
